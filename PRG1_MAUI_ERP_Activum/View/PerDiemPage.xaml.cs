23eca1272854d85f021ee90fa6a318d45f665478
08-Alexander <alexander.fallesen@icloud.com>
2026-02-04 12:05:20 +0100

    try fixing problem

--- PRG1_MAUI_ERP_Activum/View/PerDiemPage.xaml.cs
+++ PRG1_MAUI_ERP_Activum/View/PerDiemPage.xaml.cs
@@ -4,6 +4,19 @@ public partial class PerDiemPage : ContentPage
 {
 	public PerDiemPage()
 	{
-		InitializeComponent();
-	}
+		int helDag = 240;
+		int halvDag = 120;
+
+        int antalDagar = 2;
+
+        double summa = (helDag * antalDagar) + halvDag;
+
+        // hur länge
+        // varför
+        // vilket färdsätt
+        // för vad gäller utlägget
+        // hur mycket
+
+        // beräkningar på hur mycket
+    }
 }
