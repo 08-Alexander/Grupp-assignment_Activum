b12a020b769e8668472a3904aa1e22a66c25c2e8
08-Alexander <alexander.fallesen@icloud.com>
2026-02-18 13:34:00 +0100

    Traktamente klart
    sista lektionen gjorde jag klart knappen, så nu fungerar allt

--- PRG1_MAUI_ERP_Activum/View/PerDiemPage.xaml.cs
+++ PRG1_MAUI_ERP_Activum/View/PerDiemPage.xaml.cs
@@ -1,22 +1,35 @@
+using System.Threading.Tasks;
+
+
 namespace PRG1_MAUI_ERP_Activum.View;
 
 public partial class PerDiemPage : ContentPage
 {
-	public PerDiemPage()
-	{
-		int helDag = 240;
-		int halvDag = 120;
-
-        int antalDagar = 2;
-
-        double summa = (helDag * antalDagar) + halvDag;
-
-        // hur länge
-        // varför
-        // vilket färdsätt
-        // för vad gäller utlägget
-        // hur mycket
-
-        // beräkningar på hur mycket
+    public PerDiemPage()
+    {
+        InitializeComponent();
     }
+
+    public async void PerDiem_Clicked(object sender, EventArgs e)
+    {
+        string[] storage =
+        {
+            ReasonEntry.Text,
+            DaysEntry.Text,
+            DateEntry.Text,
+            EndDateEntry.Text,
+            TravelToolEntry.Text,
+            OtherExpensesEntry.Text
+        };
+
+        if (storage.Any(string.IsNullOrWhiteSpace))
+        {
+            await DisplayAlert("Fel", "Dokumentet är tomt någonstans", "OK");
+            return;
+        }
+
+        await DisplayAlert("Skickat", "Tack för att du skickade in din ansökan", "Klart");
+
+    }
+
 }
